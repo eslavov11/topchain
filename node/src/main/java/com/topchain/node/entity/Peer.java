e0ad7a540e7824055e9c333bda06dc7101ef5812
Edward <e.slavov11@gmail.com>
2018-02-09 21:15:34 +0200

    node initial POJOs

--- node/src/main/java/com/topchain/node/entity/Peer.java
+++ node/src/main/java/com/topchain/node/entity/Peer.java
@@ -1,4 +1,17 @@
 package com.topchain.node.entity;
 
+import java.net.URL;
+
 public class Peer {
+    private URL address;
+
+    public Peer() {}
+
+    public URL getAddress() {
+        return address;
+    }
+
+    public void setAddress(URL address) {
+        this.address = address;
+    }
 }
