0c03d9f12e2b7f51fb8832258b17b04f2802e034
Edward <e.slavov11@gmail.com>
2018-02-11 19:30:19 +0200

    app config

---
+++ node/src/main/java/com/topchain/node/AppConfig.java
@@ -0,0 +1,15 @@
+package com.topchain.node;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
+
+}
--- node/src/main/java/com/topchain/node/TopChainNode.java
+++ node/src/main/java/com/topchain/node/TopChainNode.java
@@ -1,18 +1,11 @@
 package com.topchain.node;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TopChainNode {
     public static void main(String[] args) {
         SpringApplication.run(TopChainNode.class, args);
-    }
-
-    @Bean
-    public ModelMapper getModelMapper() {
-        return new ModelMapper();
     }
 }
