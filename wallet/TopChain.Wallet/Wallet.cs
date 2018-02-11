4d7fc082b8599edcd557fe8198e8d0c1775c9638
PavelDochev <pointofvieww@gmail.com>
2018-02-11 18:13:13 +0200

    wallet project created

---
+++ wallet/TopChain.Wallet/Wallet.cs
@@ -0,0 +1,14 @@
+﻿namespace TopChain.Wallet
+{
+    using Org.BouncyCastle.Asn1.Sec;
+    using Org.BouncyCastle.Asn1.X9;
+    using System;
+
+    class Wallet
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PREPARE FOR WALLET");
+        }
+    }
+}
