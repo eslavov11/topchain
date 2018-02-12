6c414e34995df91bcc4501dadaffa8ace2549bf8
PavelDochev <pointofvieww@gmail.com>
2018-02-12 18:06:48 +0200

    added faucet project
    .net core + angular

---
+++ faucet/TopChain.Faucet/TopChain.Faucet/ClientApp/app/app.shared.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+//import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+//import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        //NavMenuComponent,
+        //FetchDataComponent,
+        HomeComponent
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            //{ path: 'counter', component: CounterComponent },
+            //{ path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+export class AppModuleShared {
+}
