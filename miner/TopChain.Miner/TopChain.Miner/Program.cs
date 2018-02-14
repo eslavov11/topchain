9da8de60a83752002399d8c2c225e9b281c8e3b4
PavelDochev <pointofvieww@gmail.com>
2018-02-14 12:36:33 +0200

    faster calculations
    found that cancellation token can stop all tasks

--- miner/TopChain.Miner/TopChain.Miner/Program.cs
+++ miner/TopChain.Miner/TopChain.Miner/Program.cs
@@ -46,23 +46,42 @@
 
             string minerAddress = "d08498041433013fe278f9cd63c53e6b6f0e8033";
 
-
-            var availableThreads = Environment.ProcessorCount;
-
-            var taskList = new List<Task<ResultWrapper>>();
-
-
-            ulong startingNonce = ulong.MinValue + 1;
-
-            for (int i = 0; i < availableThreads; i++)
+            while (true)
             {
-                taskList.Add(StartMining(minerAddress, startingNonce + (ulong)(5000000 / availableThreads)));
-                taskList[i].Start();
+                //use cancellation token to stop all tasks when one finishes start again..
+                //var tokenSource = new CancellationTokenSource();
+
+
+                var availableThreads = Environment.ProcessorCount;
+
+                var taskList = new List<Task<ResultWrapper>>();
+
+                GlobalCounter.Reset();
+                var sw = Stopwatch.StartNew();
+
+                ulong startingNonce = ulong.MinValue + 1;
+
+                for (int i = 0; i < availableThreads; i++)
+                {
+                    taskList.Add(Task.Factory.StartNew(() => StartMining(minerAddress, startingNonce + (ulong)(5000000 / availableThreads))));
+
+                }
+                
+                Task.WaitAll(taskList.ToArray());
+                var foundHash = taskList.FirstOrDefault(x => x.Result != null);
+
+                if (foundHash != null)
+                {
+                    SubmitPoW(foundHash.Result);
+                }
+
+                sw.Stop();
+                var kiloHashesPerSecond = (GlobalCounter.Value / sw.Elapsed.TotalSeconds) / 1000;
+                Console.WriteLine("kHs => {0} ", kiloHashesPerSecond);
+
             }
-            //TODO
-            //Start mining shouldnt be with while(true)
-            //get the while(true) outside and run the tasks
-            Task.WaitAll(taskList.ToArray());
+
+
         }
         //public static void MethodToExecute(Block blockToMine,Stopwatch sw)
         //{
@@ -70,52 +89,45 @@
         //    while (_isRunning)
         //    {
         //        
-                //todo
+        //todo
         //        if (MineBlock(blockToMine) !=null)
         //        {
         //            Console.WriteLine(sw.Elapsed.TotalSeconds); 
         //            break;
         //        }
-                
+
         //    }
         //}
 
-        public static async Task<ResultWrapper> StartMining(string minerAddress,ulong nonce)
+        public static ResultWrapper StartMining(string minerAddress, ulong nonce)
         {
             Block blockToMine = GetBlock(minerAddress);
             string blockDataHash = string.Empty;
+            ResultWrapper result = null;
 
-
-            while (true)
+            if (blockDataHash != blockToMine.BlockDataHash)
             {
-                if (blockDataHash!=blockToMine.BlockDataHash)
-                {
 
-                    GlobalCounter.Reset();
-                    var sw = Stopwatch.StartNew();
-                    blockDataHash = blockToMine.BlockDataHash;
-                    ResultWrapper result = await MineBlock(blockToMine,nonce);
-                    sw.Stop();
-                    var kiloHashesPerSecond = (GlobalCounter.Value / sw.Elapsed.TotalSeconds) / 1000;
-                    Console.WriteLine("kHs => {0} ",kiloHashesPerSecond);
-                    //SubmitPoW(result);
-                }
-                else
-                {
-                    blockToMine = GetBlock(minerAddress);
-                }
+                blockDataHash = blockToMine.BlockDataHash;
+                result = MineBlock(blockToMine, nonce);
+                //SubmitPoW(result);
             }
+            else
+            {
+                blockToMine = GetBlock(minerAddress);
+            }
+            return result;
         }
-        
 
-        public static async Task<ResultWrapper> MineBlock(Block blockToMine,ulong nonce)
+
+        public static ResultWrapper MineBlock(Block blockToMine, ulong nonce)
         {
             ResultWrapper result = new ResultWrapper();
             string dateCreated = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
             int difficulty = blockToMine.Difficulty.Value;
             var nextHash = CalcSHA256(blockToMine.BlockDataHash + dateCreated + nonce);
-
-            string nextHashSubstring = BytesToHex(nextHash).Substring(0,difficulty);
+            
+            string nextHashSubstring = BytesToHex(nextHash).Substring(0, difficulty);
             string difficultyToAchieve = string.Join(string.Empty, Enumerable.Repeat("0", difficulty).ToArray());
 
             while (nextHashSubstring != difficultyToAchieve)
@@ -123,17 +135,15 @@
                 nonce++;
                 dateCreated = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ");
                 nextHash = CalcSHA256(blockToMine.BlockDataHash + dateCreated + nonce);
-                GlobalCounter.Increment();
-
-                nextHashSubstring = BytesToHex(nextHash).Substring(0, difficulty);
                 
+                nextHashSubstring = BytesToHex(nextHash).Substring(0, difficulty);
+
             }
             result.Nonce = nonce;
             result.DateCreated = dateCreated;
 
-            Console.WriteLine(BytesToHex(nextHash));
-            Console.WriteLine(result.DateCreated);
-            Console.WriteLine(result.Nonce);
+            //Console.WriteLine(result.DateCreated);
+            //Console.WriteLine(result.Nonce);
             return result;
         }
 
@@ -141,11 +151,22 @@
         {
             byte[] bytes = Encoding.UTF8.GetBytes(text);
             Sha256Digest digest = new Sha256Digest();
-             digest.BlockUpdate(bytes, 0, bytes.Length);
+            digest.BlockUpdate(bytes, 0, bytes.Length);
             byte[] result = new byte[digest.GetDigestSize()];
             digest.DoFinal(result, 0);
+            //increment global counter
+            GlobalCounter.Increment();
             return result;
         }
+
+        //slower implementation
+        //static byte[] sha256(string randomString)
+        //{
+        //    System.Security.Cryptography.SHA256Managed crypt = new System.Security.Cryptography.SHA256Managed();
+        //    byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomString), 0, Encoding.UTF8.GetByteCount(randomString));
+           
+        //    return crypto;
+        //}
 
         public static Block GetBlock(string minerAddress)
         {
@@ -157,7 +178,7 @@
             {
                 using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
                 {
-                    client.BaseAddress = new Uri(string.Format("http://{0}{1}", nodeIP,getBlocksURL));
+                    client.BaseAddress = new Uri(string.Format("http://{0}{1}", nodeIP, getBlocksURL));
                     HttpResponseMessage response = client.GetAsync(getBlocksURL).Result;
                     response.EnsureSuccessStatusCode();
                     string responseResult = response.Content.ReadAsStringAsync().Result;
@@ -185,11 +206,40 @@
             return BitConverter.ToString(bytes).Replace("-", string.Empty).ToLower();
         }
 
-
         public static void SubmitPoW(ResultWrapper result)
         {
-            Console.WriteLine("Submited Date  {0} ",result.DateCreated);
-            Console.WriteLine("Submited Nonce  {0} ",result.Nonce);
+            Console.WriteLine("Submited Date  {0} ", result.DateCreated);
+            Console.WriteLine("Submited Nonce  {0} ", result.Nonce);
+
+
+            //string nodeIP = "127.0.0.1:5555";
+
+            //string getBlocksURL = string.Format("/mining/get-block/{0}{1}", result.DateCreated,result.Nonce);
+            //try
+            //{
+            //    using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            //    {
+            //        client.BaseAddress = new Uri(string.Format("http://{0}{1}", nodeIP, getBlocksURL));
+            //        HttpResponseMessage response = client.GetAsync(getBlocksURL).Result;
+            //        response.EnsureSuccessStatusCode();
+            //        string responseResult = response.Content.ReadAsStringAsync().Result;
+            //        //result = JsonConvert.DeserializeObject<Block>(responseResult);
+
+            //        //string signedTranJson = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+            //        //Console.WriteLine("testing serializing (JSON):");
+            //        //Console.WriteLine(signedTranJson);
+            //        //checking if its properly deserialized
+            //        //Console.WriteLine("Result: " + result[0].Index);
+            //        //result.Difficulty = 5;
+            //        //return result;
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    //return result;
+            //}
         }
     }
 }
