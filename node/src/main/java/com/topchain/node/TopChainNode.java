abcf65fda227bb988898a03fc8ca8664feeabbe8
Edward S <e.slavov11@gmail.com>
2018-03-06 17:30:13 +0200

    'node'

--- node/src/main/java/com/topchain/node/TopChainNode.java
+++ node/src/main/java/com/topchain/node/TopChainNode.java
@@ -1,11 +1,22 @@
 package com.topchain.node;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class TopChainNode {
-    public static void main(String[] args) {
-        SpringApplication.run(TopChainNode.class, args);
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class TopChainNode extends SpringBootServletInitializer {
+//    public static void main(String[] args) {
+//        SpringApplication.run(TopChainNode.class, args);
+//    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TopChainNode.class);
     }
 }
