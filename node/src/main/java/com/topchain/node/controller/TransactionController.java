35a5fc0bdf774eb9742f7bd850108256b92ff59d
Edward <e.slavov11@gmail.com>
2018-03-07 20:39:01 +0200

    notify all peers for new transaction

--- node/src/main/java/com/topchain/node/controller/TransactionController.java
+++ node/src/main/java/com/topchain/node/controller/TransactionController.java
@@ -1,5 +1,8 @@
 package com.topchain.node.controller;
 
+import com.topchain.node.entity.Node;
+import com.topchain.node.entity.Peer;
+import com.topchain.node.model.bindingModel.PeerModel;
 import com.topchain.node.model.bindingModel.TransactionModel;
 import com.topchain.node.model.viewModel.FullBalanceViewModel;
 import com.topchain.node.model.viewModel.NewTransactionViewModel;
@@ -7,19 +10,28 @@ import com.topchain.node.model.viewModel.TransactionViewModel;
 import com.topchain.node.model.viewModel.TransactionsForAddressViewModel;
 import com.topchain.node.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Set;
+
+import static com.topchain.node.util.NodeUtils.getServerURL;
+import static com.topchain.node.util.NodeUtils.serializeJSON;
 
 @RestController
 public class TransactionController {
     private TransactionService transactionService;
+    private RestTemplate restTemplate;
+    private Node node;
 
     @Autowired
-    public TransactionController(TransactionService transactionService) {
+    public TransactionController(TransactionService transactionService,
+                                 RestTemplate restTemplate,
+                                 Node node) {
         this.transactionService = transactionService;
+        this.restTemplate = restTemplate;
+        this.node = node;
     }
 
     @PostMapping("/transactions/send")
@@ -31,6 +43,11 @@ public class TransactionController {
         if (newTransactionViewModel.getTransactionHash() == null) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+
+        // Notify all peers about new transaction
+        this.node.getPeers().forEach(p ->
+                new Thread(new TransactionNotifyPeersRunnable(transactionModel, p))
+                        .start());
 
         return new ResponseEntity<>(newTransactionViewModel, HttpStatus.OK);
     }
@@ -69,5 +86,29 @@ public class TransactionController {
         //TODO: return 404 if not found
 
         return this.transactionService.getTransactionsForAddress(address);
+    }
+
+    private class TransactionNotifyPeersRunnable implements Runnable {
+        private TransactionModel transactionModel;
+        private Peer peer;
+
+        public TransactionNotifyPeersRunnable(TransactionModel transactionModel, Peer peer) {
+            this.transactionModel = transactionModel;
+        }
+
+        public void run() {
+            sendNewTransactionToPeers(this.transactionModel, this.peer);
+        }
+    }
+
+    private void sendNewTransactionToPeers(TransactionModel transactionModel, Peer peer) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> request = new HttpEntity<>(
+                serializeJSON(transactionModel, false), httpHeaders);
+
+        ResponseEntity<String> response = this.restTemplate
+                .postForEntity(peer.getUrl() + "/peers", request, String.class);
     }
 }
